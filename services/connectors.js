fee1fb50cfe99b2cb0a97684eade3ab5c4d658f5
Cristian Ovando <cristian.ovando@playtika.com>
2020-08-21 13:58:17 -0300

    mkdir

--- services/connectors.js
+++ services/connectors.js
@@ -2,9 +2,12 @@ import dotenv from 'dotenv';
 import { Storage } from '@google-cloud/storage';
 import lowdb from 'lowdb';
 import FileSync from 'lowdb/adapters/FileSync';
+import mkdirp from 'mkdirp';
 import config from '../config/config';
 
 dotenv.config();
+
+mkdirp.sync('data')
 
 const Db = lowdb(new FileSync('data/db.json'));
 Db.defaults({ videos: [] }).write();
